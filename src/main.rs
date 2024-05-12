bed9dd26aa44e3683c8a130c36c45ce5da0fcff5
Adesh Attavar <adesh.attavar@gmail.com>
2024-05-12 21:08:29 +0530

    Added functionality to read a file whose name was given as a command line arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
+    let file_contents = fs::read_to_string(file_path).expect("Expected to read the file");
+
+    println!("Contents of the file:\n{}", file_contents);
 }
