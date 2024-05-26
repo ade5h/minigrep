e2e784a299dda9dcb0f6f9c677700e1a499d07fb
ade5h <adesh.attavar@gmail.com>
2024-05-26 17:43:02 +0530

    Adding IGNORE_CASE env variable functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -14,8 +16,13 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
@@ -23,7 +30,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents =
         fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
-    let results = search(&config.query, &contents);
+    let results: Vec<&str> = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
 
     for line in results {
         println!("{}", line);
